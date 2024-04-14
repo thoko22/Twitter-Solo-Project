63de5e33e692f0cac0ef9ce9a4c78740d72ccaf4
Thokozile Mpofu <thokozilempofu22@gmail.com>
2024-04-14 04:16:55 +0200

    twitter-solo-project

---
+++ index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<head>
+    <meta charset="UTF-8" />
+    
+    <title>Parcel Sandbox</title>
+</head>
+   <h1>Access Trello Board</h1>
+   <ol>
+      <li>Use this link to access board: <a href="https://codesandbox.io/s/access-trello-board-tncyz?file=/src/index.js"></a></li>
+      <li>Click Show Menu  More  Copy Board</li>
+   </ol>
+     <script srs="src/index.js"></script>
+<body>
+    
+</body>
+`
+
+
